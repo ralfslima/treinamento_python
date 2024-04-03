43d4232571ee8a9204db717772a24695f3fba223
ralflima <ralf.lima@gmail.com>
2024-04-03 19:37:19 -0300

    Quarta aula de Python

--- aula04/exercicio04.py
+++ aula04/exercicio04.py
@@ -4,3 +4,33 @@
 
 # Lista de números
 numeros = []
+
+# Contador (Para saber quantos números diferentes temos armazenados)
+contador = 0
+
+# Laço While
+while contador < 5:
+
+    # Obter um número
+    print('Informe o '+str(contador+1)+'º número')
+    numero = int(input())
+
+    # Variável para verificar se existe o número na lista
+    existe = 0
+
+    # Laço de repetição
+    for n in numeros:
+        if n == numero:
+            existe = 1
+            break
+    
+    # Caso o número não exista
+    if existe == 0:
+        contador += 1
+        numeros.append(numero)
+    else:
+        print('O número '+str(numero)+' já foi informado!')
+
+# Exibir os valores sem repetição
+for n in numeros:
+    print(n)
